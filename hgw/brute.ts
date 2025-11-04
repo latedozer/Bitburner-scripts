5a4fd122edbabcff015170f01df63ace26cb79a5
latedozer <dozers.domain1@gmail.com>
2025-11-04 13:35:22 -0800

    brute fix

--- hgw/brute.ts
+++ hgw/brute.ts
@@ -1,5 +1,5 @@
 import { NS } from '@ns';
-import * as c.filePath(from './c)onsts.js';
+import * as c from './consts.js';
 
 const delay = 20;
 const hacks = 1;
@@ -11,6 +11,7 @@ const weakTh = 3;
 const hackPath = c.filePath(c.hackFile);
 const growPath = c.filePath(c.growFile);
 const weakPath = c.filePath(c.weakFile);
+const constPath = c.filePath(c.constFile);
 export async function main(ns: NS) {
 
   while (true) {
@@ -19,18 +20,18 @@ export async function main(ns: NS) {
     let hostServ: c.max = await ns.peek(c.hostServ);
     let bestServ: c.max = await ns.peek(c.bestServ);
     if (ns.getServerMaxRam(hostServ.name) - (ns.getServerUsedRam(hostServ.name)) > ramCost * 10) {
-      if (!ns.fileExists(c.filePath(c.constFile), hostServ.name) || !ns.fileExists(c.filePath(c.weakFile), hostServ.name) || !ns.fileExists(c.filePath(c.growFile) hostServ.name) || !ns.fileExists(c.filePath(hackFile), hostServ.name)) {
-        ns.scp([c.filePath(constFile), c.filePath(hackFile,) c.filePath(growFile,) c.filePath(weakFile]), hostServ.name, "home");
+      if (!ns.fileExists(constPath, hostServ.name) || !ns.fileExists(c.filePath(c.weakFile), hostServ.name) || !ns.fileExists(c.filePath(c.growFile), hostServ.name) || !ns.fileExists(hackPath, hostServ.name)) {
+        ns.scp([constPath, hackPath, growPath, weakPath], hostServ.name, "home");
       }
       for (let i = 0; i < 10; i++) {
-        ns.exec(c.filePath(hackFile,) hostServ.name, hackTh, bestServ.name);
+        ns.exec(hackPath, hostServ.name, hackTh, bestServ.name);
         await ns.sleep(delay);
         for (let y = 0; y < grows; y++) {
-          ns.exec(c.filePath(growFile,) hostServ.name, growTh, bestServ.name);
+          ns.exec(growPath, hostServ.name, growTh, bestServ.name);
           await ns.sleep(delay);
         }
         for (let y = 0; y < weaks; y++) {
-          ns.exec(c.filePath(weakFile,) hostServ.name, weakTh, bestServ.name);
+          ns.exec(weakPath, hostServ.name, weakTh, bestServ.name);
           await ns.sleep(delay);
         }
       }
