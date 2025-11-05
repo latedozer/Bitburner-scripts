9c2760a709b00787de324c8c3647c3b97bfa3f04
latedozer <dozers.domain1@gmail.com>
2025-11-05 02:27:55 -0800

    fixed fatal bug

---
+++ hgw/serverBuy.ts
@@ -0,0 +1,22 @@
+import { NS } from '@ns';
+import * as c from './consts.js';
+
+export async function main(ns: NS) {
+    let i = 0;
+    if (!ns.serverExists("host")) {
+        do {
+            i++
+        } while (ns.getPurchasedServerCost(2 ** (i + 1)) < ns.getPlayer().money && i < 21);
+        if (i > 32) {
+            ns.purchaseServer("host", 2 ** i);
+        }
+        return;
+    }
+    if(ns.getServer("host").maxRam === 2**20){
+        ns.toast("there's no more ram to buy for host server");
+    }
+    do {
+        i++
+    } while (ns.getPurchasedServerUpgradeCost("host", 2 ** (i + 1)) < ns.getPlayer().money && i < 21);
+    ns.upgradePurchasedServer("host", 2**i)
+}
